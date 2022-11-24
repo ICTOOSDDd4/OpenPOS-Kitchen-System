8be3ca4d3113aca7bca914a4ce5913b79bca38f1
Renske3 <74046258+Renske3@users.noreply.github.com>
2022-11-24 12:14:55 +0100

    menu weergave verbeterd

--- OpenPOS-APP/RealisatieMenu.xaml.cs
+++ OpenPOS-APP/RealisatieMenu.xaml.cs
@@ -12,10 +12,10 @@ public partial class RealisatieMenu : ContentPage {
         List<Product> ProductList = new List<Product>();
 
         ProductList.Add(new Product(1, "Spaghetti", 2.25, "Spaghetti met tomatensaus", "spaghetti.png"));
-        ProductList.Add(new Product(2, "Tomatensoep", 3.50, "Soep met tomaat", "Unknown"));
-        ProductList.Add(new Product(3, "Boerenkool", 4.25, "Aardappels met boerenkool", "Unknown"));
-        ProductList.Add(new Product(4, "Kipnuggets", 1.50, "Nuggets met kip", "Unknown"));
-        ProductList.Add(new Product(5, "Hamburger", 1.75, "Burger met ham", "Unknown"));
+        ProductList.Add(new Product(2, "Tomatensoep", 3.55, "Soep met tomaat", "Tomatensoep.png"));
+        ProductList.Add(new Product(3, "Stamppot Boerenkool", 4.25, "Aardappels met boerenkool", "Boerenkool.png"));
+        ProductList.Add(new Product(4, "Kipnuggets", 1.55, "Nuggets met kip", "kipnuggets.png"));
+        ProductList.Add(new Product(5, "Hamburger", 1.75, "Burger met ham", "hamburger.png"));
         RealisatieMenuItems.ItemsSource = ProductList;
     }
 
