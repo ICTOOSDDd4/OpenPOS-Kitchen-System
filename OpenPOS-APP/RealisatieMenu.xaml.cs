430f86f1a78e95d88f3d3cd35973e10c3b09a9b8
Renske3 <74046258+Renske3@users.noreply.github.com>
2022-11-24 12:40:25 +0100

    Menu realisatie

--- OpenPOS-APP/RealisatieMenu.xaml.cs
+++ OpenPOS-APP/RealisatieMenu.xaml.cs
@@ -9,13 +9,13 @@ namespace OpenPOS_APP;
 public partial class RealisatieMenu : ContentPage {
     public RealisatieMenu(){
         InitializeComponent();
-        List<Product> ProductList = new List<Product>();
-
-        ProductList.Add(new Product(1, "Spaghetti", 2.25, "Spaghetti met tomatensaus", "spaghetti.png"));
-        ProductList.Add(new Product(2, "Tomatensoep", 3.55, "Soep met tomaat", "Tomatensoep.png"));
-        ProductList.Add(new Product(3, "Stamppot Boerenkool", 4.25, "Aardappels met boerenkool", "Boerenkool.png"));
-        ProductList.Add(new Product(4, "Kipnuggets", 1.55, "Nuggets met kip", "kipnuggets.png"));
-        ProductList.Add(new Product(5, "Hamburger", 1.75, "Burger met ham", "hamburger.png"));
+        List<Product> ProductList = new List<Product> {
+            new Product(1, "Spaghetti", 2.25, "Spaghetti met tomatensaus", "spaghetti.png"),
+            new Product(2, "Tomatensoep", 3.50, "Soep met tomaat", "Tomatensoep.png"),
+            new Product(3, "Boerenkool", 4.25, "Aardappels met boerenkool", "Boerenkool.png"),
+            new Product(4, "Kipnuggets", 1.50, "Nuggets met kip", "kipnuggets.png"),
+            new Product(5, "Hamburger", 1.75, "Burger met ham", "hamburger.png")
+        };
         RealisatieMenuItems.ItemsSource = ProductList;
     }
 
